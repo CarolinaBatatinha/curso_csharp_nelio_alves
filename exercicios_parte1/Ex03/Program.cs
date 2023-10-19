9d39b3b6fd5777614985d7e246453ebac8205bd5
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-10-19 16:59:20 -0300

    Adicionado o arquivo Ex03

---
+++ exercicios_parte1/Ex03/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Exemplos:
+Entrada:         Saída:
+ 5               DIFERENCA = -26
+ 6
+ 7
+ 8
+
+Entrada:        Saída:
+ 5              DIFERENCA = 86
+ 6
+-7
+ 8
+ */
+using System;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor inteiro para A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor inteiro para B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor inteiro para C: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor inteiro para D: ");
+            int d = int.Parse(Console.ReadLine());
+
+            int diferenca = (a * b) - (c * d);
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}

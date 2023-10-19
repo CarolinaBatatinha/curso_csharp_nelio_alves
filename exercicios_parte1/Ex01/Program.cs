260f0426d1bd5b7f8f200bc044c6416687e1eb1e
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-10-19 12:47:21 -0300

    Inclusão da pasta 'exercicios_parte1', bem como dos exercícios Ex01 e Ex02

---
+++ exercicios_parte1/Ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos. Exemplos:
+//Entrada:            Saída:
+//10                  SOMA = 40
+//30
+
+//Entrada:            Saída:
+//-30                 SOMA = -20
+//10
+
+//Entrada:            Saída:
+//0                   SOMA = 0
+//0
+
+using System;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número inteiro: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número inteiro: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"SOMA = {n1 + n2}");
+        }
+    }
+}

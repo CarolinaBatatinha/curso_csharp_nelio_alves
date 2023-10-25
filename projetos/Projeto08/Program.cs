d03db480fc570f5f82ca67ea48f0d46311e9b5ef
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-10-25 17:05:53 -0300

    Envio do arquivo Projeto08

---
+++ projetos/Projeto08/Program.cs
@@ -0,0 +1,36 @@
+﻿// Faça um prgrama para ler tres números inteiros e mostrar na tela o maior deles
+
+using System;
+
+class Program
+{
+    static void Main(string[] args) // entry  point
+    {
+        Console.WriteLine("Digite três números inteiros: ");
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+
+       double resultado = Maior(n1, n2, n3);
+       Console.WriteLine($"Maior = {resultado}");
+
+    }
+
+    static int Maior(int a, int b, int c)
+    { 
+        int m;
+        if (a > b && a > c)
+        {
+            m = a;
+        }
+        else if ( b > c)
+        {
+            m = b;
+        }
+        else
+        {
+            m = c;
+        }
+        return m;
+    }
+}
